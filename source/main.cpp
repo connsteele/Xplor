3545c0277a9a51a1950c6aea7accdd24d87e310c
connsteele <35544211+connsteele@users.noreply.github.com>
2023-10-16 21:50:33 -0700

    Render loop with color clearing

--- source/main.cpp
+++ source/main.cpp
@@ -11,9 +11,20 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+// Handle all incoming keyboard and mouse events
+void processInputs(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        // Enable the close window flag
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 
 int main(int argc, char **argv) {
     //---- Setup dependencies ----
+    //-----------------------------------------------------
     if (!glfwInit())
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
@@ -22,6 +33,7 @@ int main(int argc, char **argv) {
 
 
     //---- Set the attributes for the window ----
+    //-----------------------------------------------------
     // Set the context to use OpenGL 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -55,9 +67,32 @@ int main(int argc, char **argv) {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    //---- Render Loop ----
+    //-----------------------------------------------------
+    while (!glfwWindowShouldClose(window)) // Render till the close flag is true
+    {
+        //--- Input
+        //-----------------------------------------------------
+        processInputs(window);
+
+
+        // Rendering commands
+        //-----------------------------------------------------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // Set the clear color
+        glClear(GL_COLOR_BUFFER_BIT); // Clear the color buffer with the defined color
+
+        
+        // Swap the front and back buffers
+        glfwSwapBuffers(window);
+        // Process pending events and update the window state
+        glfwPollEvents();
+    }
+
     
-    // Cleanup
+    //---- Cleanup ----
+    //-----------------------------------------------------
     glfwDestroyWindow(window);
-    // glfwTerminate();
+    glfwTerminate();
     return 0;
 }
